9b2b47ee1180d8c8ac7c287b0f88d1820afad43b
kuramu1108 <colagarychen@gmail.com>
2018-03-06 20:41:47 +1100

    firebase Auth and pin activity init

---
+++ app/src/main/java/com/kuramu/splitbill/Activity/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.kuramu.splitbill.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.kuramu.splitbill.Model.Customer
+import com.kuramu.splitbill.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var user: Customer? = null
+    var mAuth:FirebaseAuth? = null
+    var mUser:FirebaseUser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        //user = Customer(id = 1, clientNumber = "PT445")
+
+        btn_login.setOnClickListener {
+            mAuth!!.signInWithEmailAndPassword(txt_email.text.toString(), txt_password.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(applicationContext, "Signed in", Toast.LENGTH_LONG).show()
+                            mUser = mAuth!!.currentUser
+                            updateUI(mUser!!)
+                        } else {
+                            Toast.makeText(applicationContext, "Logged in failed", Toast.LENGTH_LONG).show()
+                        }
+
+                    }
+
+            //Toast.makeText(applicationContext, "Submitted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun updateUI(mUser:FirebaseUser) {
+
+    }
+
+}
---
+++ app/src/main/java/com/kuramu/splitbill/Activity/PinActivity.kt
@@ -0,0 +1,14 @@
+package com.kuramu.splitbill.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kuramu.splitbill.R
+
+class PinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+
+    }
+}
